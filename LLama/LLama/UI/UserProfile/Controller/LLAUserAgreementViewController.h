900b029f8f97e0d826f22444391da53b91d2100b
tommin <tomming67@foxmail.com>
2016-01-30 14:46:14 +0800

    加入用户协议

---
+++ LLama/LLama/UI/UserProfile/Controller/LLAUserAgreementViewController.h
@@ -0,0 +1,14 @@
+//
+//  LLAUserAgreementViewController.h
+//  LLama
+//
+//  Created by tommin on 16/1/30.
+//  Copyright © 2016年 heihei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "LLACommonViewController.h"
+
+@interface LLAUserAgreementViewController : LLACommonViewController
+
+@end
