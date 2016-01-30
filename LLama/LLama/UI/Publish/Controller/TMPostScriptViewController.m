10dd34e8122f89afe2ab4c7177326e3f8b40f07a
李常聪 <332934863@qq.com>
2016-01-30 14:19:18 +0800

    发布剧本

--- LLama/LLama/UI/Publish/Controller/TMPostScriptViewController.m
+++ LLama/LLama/UI/Publish/Controller/TMPostScriptViewController.m
@@ -8,7 +8,19 @@
 
 #import "TMPostScriptViewController.h"
 
+#import "LLATextView.h"
+
 @interface TMPostScriptViewController ()
+
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *textViewTrailConstraint;
+@property (weak, nonatomic) IBOutlet UITextField *rewardMoneyTextField;
+@property (weak, nonatomic) IBOutlet UIImageView *preViewImageView;
+@property (weak, nonatomic) IBOutlet LLATextView *scriptContentTextView;
+@property (weak, nonatomic) IBOutlet UIButton *isPrivateButton;
+@property (weak, nonatomic) IBOutlet UIButton *weChatFriend;
+@property (weak, nonatomic) IBOutlet UIButton *weChatTimeLine;
+@property (weak, nonatomic) IBOutlet UIButton *sinaWeiBo;
+@property (weak, nonatomic) IBOutlet UIButton *qqFriend;
 
 @end
 
@@ -21,21 +33,75 @@
     // 设置标题
     self.navigationItem.title = @"剧本";
     
+    //
+    _rewardMoneyTextField.placeholder = @"点击此处偷入金额";
+    _rewardMoneyTextField.textAlignment = NSTextAlignmentRight;
+    //
+    _preViewImageView.contentMode = UIViewContentModeScaleAspectFill;
     
-    [self.navigationController.navigationBar setTitleTextAttributes:
-     @{NSFontAttributeName:[UIFont systemFontOfSize:18],
-       NSForegroundColorAttributeName:[UIColor whiteColor]}];
+    //
+    _scriptContentTextView.placeholder = @"这里写剧本";
     
-    // 设置导航栏左边的按钮
-    self.navigationItem.leftBarButtonItem = [UIBarButtonItem itemWithImage:@"back" highImage:@"backh" target:self action:@selector(backButtonClick)];
+    //
+    [_isPrivateButton setImage:[UIImage llaImageWithName:@"public"] forState:UIControlStateNormal];
+    [_isPrivateButton setImage:[UIImage llaImageWithName:@"privite"] forState:UIControlStateHighlighted];
+    [_isPrivateButton setImage:[UIImage llaImageWithName:@"privite"] forState:UIControlStateSelected];
+    _isPrivateButton.tintColor = nil;
     
-    // 设置导航栏背景颜色
-    [self.navigationController.navigationBar setBackgroundImage:[UIImage imageNamed:@"tabbar_bg"] forBarMetrics:UIBarMetricsDefault];
+    [_isPrivateButton addTarget:self action:@selector(isPrivateButtonClicked:) forControlEvents:UIControlEventTouchUpInside];
+    
+    //
+    [_weChatFriend setImage:[UIImage llaImageWithName:@"wechatShare"] forState:UIControlStateNormal];
+    [_weChatFriend setImage:[UIImage llaImageWithName:@"wechatShareHighlight"] forState:UIControlStateSelected];
+    _weChatFriend.tintColor = nil;
+    
+    [_weChatFriend addTarget:self action:@selector(weChatFriendClicked:) forControlEvents:UIControlEventTouchUpInside];
+    
+    [_weChatTimeLine setImage:[UIImage llaImageWithName:@"wechatcircle"] forState:UIControlStateNormal];
+    [_weChatTimeLine setImage:[UIImage llaImageWithName:@"wechatcircleh"] forState:UIControlStateSelected];
+    _weChatFriend.tintColor = nil;
+    
+    [_weChatTimeLine addTarget:self action:@selector(weChatTimeLineClicked:) forControlEvents:UIControlEventTouchUpInside];
+    
+    [_sinaWeiBo setImage:[UIImage llaImageWithName:@"weiboShare"] forState:UIControlStateNormal];
+    [_sinaWeiBo setImage:[UIImage llaImageWithName:@"weiboShareHighlight"] forState:UIControlStateSelected];
+    
+    _sinaWeiBo.tintColor = nil;
+    [_sinaWeiBo addTarget:self action:@selector(sinaWeiBoClicked:) forControlEvents:UIControlEventTouchUpInside];
+    
+    [_qqFriend setImage:[UIImage llaImageWithName:@"qqtiny"] forState:UIControlStateNormal];
+    [_qqFriend setImage:[UIImage llaImageWithName:@"qqtinyh"] forState:UIControlStateSelected];
+    _qqFriend.tintColor = nil;
+    [_qqFriend addTarget:self action:@selector(qqFriendClicked:) forControlEvents:UIControlEventTouchUpInside];
+}
+
+#pragma mark - Button Touch up Inside
+
+- (void) isPrivateButtonClicked:(UIButton *) sender {
+    
+    _isPrivateButton.selected = !_isPrivateButton.selected;
+}
+
+- (void) weChatFriendClicked:(UIButton *) sender {
+    _weChatFriend.selected = !_weChatFriend.selected;
+}
+
+- (void) weChatTimeLineClicked:(UIButton *) sender {
+    _weChatTimeLine.selected = !_weChatTimeLine.selected;
+}
+
+- (void) sinaWeiBoClicked:(UIButton *) sender {
+    _sinaWeiBo.selected = !_sinaWeiBo.selected;
+}
+
+
+- (void) qqFriendClicked:(UIButton *) sender {
+    _qqFriend.selected = !_qqFriend.selected;
 }
 
 - (void)backButtonClick
 {
-    [self dismissViewControllerAnimated:YES completion:nil];
+    [self.navigationController dismissViewControllerAnimated:YES completion:nil];
 }
 
 
